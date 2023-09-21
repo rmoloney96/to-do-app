ac8cd7ba298bdf2904629f811119ed4c97f7d1f8
Rob Moloney <rmoloney@tcd.ie>
2023-09-21 12:31:26 +0100

    Adding client files to Git

---
+++ src/components/Modal.js
@@ -0,0 +1,93 @@
+import {useState} from 'react';
+import {useCookies} from 'react-cookie';
+
+const Modal = ({mode, setShowModal, task, getData}) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const editMode = mode === 'edit' ? true : false;
+
+  const [data, setData] = useState({
+    user_email: editMode ? task.user_email : cookies.Email,
+    title: editMode ? task.title : null,
+    progress: editMode ? task.progress : 50,
+    date: editMode ? task.date: new Date()
+  })
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setData(data => ({
+      ...data,
+      [name] : value
+    }))
+  }
+
+  const postData = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      })
+      if(response.status === 200) {
+        console.log('Worked!');
+        setShowModal(false);
+        getData();
+      }
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  const editData = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${task.id}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      })
+      if(response.status === 200) {
+        setShowModal(false);
+        getData();
+      }
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+    return (
+      <div className="overlay">
+        <div className="modal">
+          <div className="form-title-container">
+            <h3>Let's {mode} your task</h3>
+            <button onClick={() => setShowModal(false)}>X</button>
+          </div>
+          <form>
+            <input 
+              required
+              maxLength={30}
+              placeholder=" Your task goes here"
+              name="title"
+              value={data.title}
+              onChange={handleChange}
+            />
+            <br/>
+            <label for="range">Drag to select your current progress</label>
+            <input 
+              required
+              type="range"
+              id="range"
+              min="0"
+              max="100"
+              name="progress"
+              defaultValue={data.progress}
+              onChange={handleChange} 
+            />
+            <input className={mode} type="submit" onClick={editMode ? editData : postData}/>
+          </form>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Modal;
